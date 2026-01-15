8f4e6219b949aa294098d4cc2cde61d736456b67
Denys Ivashchenko <ce-denys.ivashchenko@regnology.net>
2026-01-16 00:12:54 +0200

    docker

--- testAutomation/main.py
+++ testAutomation/main.py
@@ -1,21 +1,15 @@
 import asyncio
-import sys
-
 from health import health_check
-from auto_run import run_test
-
+import asyncio
 
 def main():
     print("🔍 Health check...")
     ok = asyncio.run(health_check())
 
     if not ok:
-        print("❌ Health check failed")
-        sys.exit(1)
+        raise SystemExit(1)
 
-    print("🚀 Running tests...")
-    run_test()
-
+    print("✅ Automation finished successfully")
 
 if __name__ == "__main__":
     main()
